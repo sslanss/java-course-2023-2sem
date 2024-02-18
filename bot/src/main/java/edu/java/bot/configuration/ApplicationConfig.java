a0c6e6b275186c391536dfbb45e9dc3714f42c5c
sslanss <sidel.sveta@gmail.com>
2024-02-19 01:18:07 +0300

    hw1-fixes

--- bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
+++ bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
@@ -1,9 +1,6 @@
 package edu.java.bot.configuration;
 
 import jakarta.validation.constraints.NotEmpty;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
